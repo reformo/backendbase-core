1dbd14ec7922ca0b6c5e1af94dbe21cdad31136c
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2021-02-04 23:22:22 +0300

    additinal twig globals middleware added

---
+++ src/BackendBase/Shared/Middleware/TemplateDefaultsMiddleware.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace BackendBase\Shared\Middleware;
+
+
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Mezzio\Template\TemplateRendererInterface;
+use Selami\Stdlib\Git\Version;
+
+class TemplateDefaultsMiddleware implements MiddlewareInterface
+{
+    private TemplateRendererInterface $templateRenderer;
+    private array $config;
+    public function __construct(TemplateRendererInterface $templateRenderer, array $config)
+    {
+        $this->templateRenderer = $templateRenderer;
+        $this->config = $config;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface
+    {
+
+        $this->templateRenderer->addDefaultParam(
+            TemplateRendererInterface::TEMPLATE_ALL,
+            'baseUrl',
+            $request->getAttribute('base-url')
+        );
+
+        $request = $request->withAttribute('cdn-url', $this->config['app']['cdn-url']);
+
+
+        $userAgent = $request->getHeaderLine('Accept');
+        $isWebP = 0;
+        if (stripos($userAgent, 'webp') !== false) {
+            $isWebP = 1;
+        }
+        $this->templateRenderer->addDefaultParam(
+            TemplateRendererInterface::TEMPLATE_ALL,
+            'isWebP',
+            $isWebP
+        );
+
+        $userAgent = $request->getHeaderLine('user-agent');
+        $isLightHouse = 0;
+        if (stripos($userAgent, 'lighthouse') !== false) {
+            $isLightHouse = 1;
+        }
+        $this->templateRenderer->addDefaultParam(
+            TemplateRendererInterface::TEMPLATE_ALL,
+            'isLightHouse',
+            $isLightHouse
+        );
+        $gitVersion = Version::short();
+        $this->templateRenderer->addDefaultParam(
+            TemplateRendererInterface::TEMPLATE_ALL,
+            'gitVersion',
+            $gitVersion
+        );
+        return $handler->handle($request);
+    }
+}
---
+++ src/BackendBase/Shared/Middleware/TemplateDefaultsMiddlewareFactory.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace BackendBase\Shared\Middleware;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Http\Server\MiddlewareInterface;
+
+final class TemplateDefaultsMiddlewareFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): MiddlewareInterface
+    {
+        return new TemplateDefaultsMiddleware($container->get(TemplateRendererInterface::class), $container->get('config'));
+    }
+}
