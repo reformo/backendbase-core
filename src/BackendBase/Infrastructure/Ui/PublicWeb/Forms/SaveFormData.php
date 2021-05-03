f3f8e81bb6fbe8543a96a5b1b815e191a64a9971
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2021-05-03 08:30:39 +0300

    saveform data updated

--- src/BackendBase/Infrastructure/Ui/PublicWeb/Forms/SaveFormData.php
+++ src/BackendBase/Infrastructure/Ui/PublicWeb/Forms/SaveFormData.php
@@ -6,6 +6,7 @@ namespace BackendBase\PublicWeb\Forms;
 
 use BackendBase\Infrastructure\Persistence\Doctrine\Entity\FormData;
 use BackendBase\Infrastructure\Persistence\Doctrine\Repository\GenericRepository;
+use BackendBase\Shared\Middleware\SessionMiddleware;
 use BackendBase\Shared\Services\FlashMessages;
 use BackendBase\Shared\Services\PayloadSanitizer;
 use DateTimeImmutable;
@@ -14,6 +15,7 @@ use Mezzio\Helper\ServerUrlHelper;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use PSR7Sessions\Storageless\Session\SessionInterface;
 use Ramsey\Uuid\Uuid;
 
 class SaveFormData implements RequestHandlerInterface
@@ -32,10 +34,20 @@ class SaveFormData implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $requestParameters = PayloadSanitizer::sanitize($request->getParsedBody());
+        /**
+         * @var $session SessionInterface
+         */
+        $session = $request->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE);
+        $captcha = $session->get('__captcha');
+        $session->remove('__captcha');
+        if (!empty($captcha) && $captcha !== $requestParameters['__captcha']??null) {
+            $flash = $request->getAttribute(FlashMessages::FLASH_MESSAGE_ATTRIBUTE);
+            $flash->flash('formData', $requestParameters);
+            return new RedirectResponse($this->urlHelper->generate() . '?r=error&m=captcha', 302);
+        }
         if (empty($requestParameters['message'])) {
             $flash = $request->getAttribute(FlashMessages::FLASH_MESSAGE_ATTRIBUTE);
-            $flash->flash('contactFormData', $requestParameters);
-
+            $flash->flash('formData', $requestParameters);
             return new RedirectResponse($this->urlHelper->generate() . '?r=error', 302);
         }
 
