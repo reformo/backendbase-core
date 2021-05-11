e46ebbe115cd99a0648970c9ec1adc87d28ce4cb
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2021-05-11 10:54:56 +0300

    cs-fix

--- src/BackendBase/Infrastructure/Ui/PublicWeb/Forms/SaveFormData.php
+++ src/BackendBase/Infrastructure/Ui/PublicWeb/Forms/SaveFormData.php
@@ -10,13 +10,11 @@ use BackendBase\Shared\Middleware\SessionMiddleware;
 use BackendBase\Shared\Services\FlashMessages;
 use BackendBase\Shared\Services\PayloadSanitizer;
 use DateTimeImmutable;
-use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\Response\RedirectResponse;
 use Mezzio\Helper\ServerUrlHelper;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use PSR7Sessions\Storageless\Session\SessionInterface;
 use Ramsey\Uuid\Uuid;
 
 class SaveFormData implements RequestHandlerInterface
@@ -38,18 +36,21 @@ class SaveFormData implements RequestHandlerInterface
         /**
          * @var $session SessionInterface
          */
-        $session = $request->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE);
-        $captcha = $session->get('__captcha');
+        $session        = $request->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE);
+        $captcha        = $session->get('__captcha');
         $requestCaptcha = $requestParameters['__captcha'] ?? null;
         $session->remove('__captcha');
         if (empty($captcha) || empty($requestCaptcha) || $captcha !== $requestCaptcha) {
             $flash = $request->getAttribute(FlashMessages::FLASH_MESSAGE_ATTRIBUTE);
             $flash->flash('formData', $requestParameters);
+
             return new RedirectResponse($this->urlHelper->generate() . '?r=error&m=captcha', 302);
         }
+
         if (empty($requestParameters['message'])) {
             $flash = $request->getAttribute(FlashMessages::FLASH_MESSAGE_ATTRIBUTE);
             $flash->flash('formData', $requestParameters);
+
             return new RedirectResponse($this->urlHelper->generate() . '?r=error', 302);
         }
 
